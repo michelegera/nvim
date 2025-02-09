7a1f8acf7e128147048c2e5ce7f3b668f39d55b5
Michele Gerarduzzi <hello@michelegera.dev>
2025-02-09 18:50:12 +0100

    feat: remove separators in alpha dashboard

--- lua/michelegera/plugins/alpha.lua
+++ lua/michelegera/plugins/alpha.lua
@@ -24,12 +24,12 @@ return {
 
     -- Set the buttons section of the dashboard
     dashboard.section.buttons.val = {
-      dashboard.button('e', '  > New File', '<cmd>ene<CR>'),
-      dashboard.button('\\', '  > Toggle File Explorer', '<cmd>Neotree reveal<CR>'),
-      dashboard.button('SPC ff', '󰱼  > Find File', '<cmd>Telescope find_files<CR>'),
-      dashboard.button('SPC fg', '  > Find by Grep', '<cmd>Telescope live_grep<CR>'),
-      dashboard.button('SPC wr', '󰁯  > Restore Session For Current Directory', '<cmd>SessionRestore<CR>'),
-      dashboard.button('q', '  > Quit NVIM', '<cmd>qa<CR>'),
+      dashboard.button('e', '  New File', '<cmd>ene<CR>'),
+      dashboard.button('\\', '  Toggle File Explorer', '<cmd>Neotree reveal<CR>'),
+      dashboard.button('SPC ff', '󰱼  Find File', '<cmd>Telescope find_files<CR>'),
+      dashboard.button('SPC fg', '  Find by Grep', '<cmd>Telescope live_grep<CR>'),
+      dashboard.button('SPC wr', '󰁯  Restore Session For Current Directory', '<cmd>SessionRestore<CR>'),
+      dashboard.button('q', '  Quit NVIM', '<cmd>qa<CR>'),
     }
 
     -- Setup alpha with the dashboard options
