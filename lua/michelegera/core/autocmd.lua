1fa6c3a5c42816cd567ef75881f2842e2767aee8
Michele Gerarduzzi <hello@michelegera.dev>
2025-02-09 14:45:27 +0100

    feat: add autocommands

---
+++ lua/michelegera/core/autocmd.lua
@@ -0,0 +1,10 @@
+local autocmd = vim.api.nvim_create_autocmd
+local augroup = vim.api.nvim_create_augroup
+
+autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = augroup('highlight-yank', { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
--- lua/michelegera/core/init.lua
+++ lua/michelegera/core/init.lua
@@ -1,3 +1,4 @@
+require 'michelegera.core.autocmd'
 require 'michelegera.core.globals'
 require 'michelegera.core.keymaps'
 require 'michelegera.core.options'
