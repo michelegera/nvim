30c9df0720598cc2a50fad3368f47eb62770e281
Michele Gerarduzzi <hello@michelegera.dev>
2025-02-09 17:43:47 +0100

    chore: format autopairs configuration

--- lua/michelegera/plugins/autopairs.lua
+++ lua/michelegera/plugins/autopairs.lua
@@ -3,10 +3,8 @@
 
 return {
   'windwp/nvim-autopairs',
-  event = { 'InsertEnter' },
-  dependencies = {
-    'hrsh7th/nvim-cmp',
-  },
+  event = 'InsertEnter',
+  dependencies = { 'hrsh7th/nvim-cmp' },
   config = function()
     local autopairs = require 'nvim-autopairs'
 
