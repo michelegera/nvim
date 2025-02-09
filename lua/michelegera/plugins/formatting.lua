52cac78c578d957cd9ec77c784fccd399b240bcb
Michele Gerarduzzi <hello@michelegera.dev>
2025-02-09 17:24:52 +0100

    feat: update Conform configuration

--- lua/michelegera/plugins/formatting.lua
+++ lua/michelegera/plugins/formatting.lua
@@ -3,23 +3,23 @@
 
 return {
   'stevearc/conform.nvim',
-  event = { 'BufReadPre', 'BufNewFile' },
+  event = { 'BufWritePre' },
   config = function()
     local conform = require 'conform'
 
     conform.setup {
       formatters_by_ft = {
-        javascript = { 'prettier' },
-        typescript = { 'prettier' },
-        javascriptreact = { 'prettier' },
-        typescriptreact = { 'prettier' },
         css = { 'prettier' },
-        html = { 'prettier' },
-        json = { 'prettier' },
-        yaml = { 'prettier' },
-        markdown = { 'prettier' },
         graphql = { 'prettier' },
+        html = { 'prettier' },
+        javascript = { 'prettier' },
+        javascriptreact = { 'prettier' },
+        json = { 'prettier' },
         lua = { 'stylua' },
+        markdown = { 'prettier' },
+        typescript = { 'prettier' },
+        typescriptreact = { 'prettier' },
+        yaml = { 'prettier' },
       },
       format_on_save = {
         lsp_fallback = true,
@@ -31,7 +31,7 @@ return {
     vim.keymap.set({ 'n', 'v' }, '<leader>mp', function()
       conform.format {
         lsp_fallback = true,
-        async = false,
+        async = true,
         timeout_ms = 1000,
       }
     end, { desc = 'Format file or range (in visual mode)' })
