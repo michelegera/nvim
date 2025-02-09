78cf5307e048e5d1a7d9dfd355162834beea5230
Michele Gerarduzzi <hello@michelegera.dev>
2025-02-09 12:08:18 +0100

    ✨ Add mason-tool-installer

--- lua/michelegera/plugins/lsp/mason.lua
+++ lua/michelegera/plugins/lsp/mason.lua
@@ -2,10 +2,12 @@ return {
   'williamboman/mason.nvim',
   dependencies = {
     'williamboman/mason-lspconfig.nvim',
+    'WhoIsSethDaniel/mason-tool-installer.nvim',
   },
   config = function()
     local mason = require 'mason'
     local mason_lspconfig = require 'mason-lspconfig'
+    local mason_tool_installer = require 'mason-tool-installer'
 
     mason.setup {
       ui = {
@@ -25,6 +27,13 @@ return {
         'html',
         'lua_ls',
         'ts_ls',
+      },
+    }
+
+    mason_tool_installer.setup {
+      ensure_installed = {
+        'prettier',
+        'stylua',
       },
     }
   end,
