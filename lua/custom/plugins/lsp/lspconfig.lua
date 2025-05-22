a0273842e345061631920cc7573eea167d768959
Michele Gerarduzzi <hello@michelegera.dev>
2025-05-22 22:44:24 +0200

    🚚 Update diagnostic sign icons definition

--- lua/custom/plugins/lsp/lspconfig.lua
+++ lua/custom/plugins/lsp/lspconfig.lua
@@ -47,11 +47,16 @@ return {
     local capabilities = cmp_nvim_lsp.default_capabilities()
     local signs = { Error = ' ', Warn = ' ', Hint = '󰠠 ', Info = ' ' }
 
-    for type, icon in pairs(signs) do
-      local hl = 'DiagnosticSign' .. type
-
-      vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = '' })
-    end
+    vim.diagnostic.config({
+      signs = {
+        text = {
+          [vim.diagnostic.severity.ERROR] = signs.Error,
+          [vim.diagnostic.severity.WARN]  = signs.Warn,
+          [vim.diagnostic.severity.HINT]  = signs.Hint,
+          [vim.diagnostic.severity.INFO]  = signs.Info,
+        },
+      },
+    })
 
     mason_lspconfig.setup_handlers {
       function(server_name)
